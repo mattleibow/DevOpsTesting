53d5d84272d35807baee7d06b5e3488a0c4039dc
Matthew Leibowitz <mattleibow@live.com>
2019-10-25 01:07:16 -0400

    Update install-vs.ps1

--- install-vs.ps1
+++ install-vs.ps1
@@ -1,4 +1,5 @@
 $path = & "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath
+$config = Join-Path $pwd ".vsconfig"
 Write-Host "Found Visual Studio at: $path"
 
 $installer = Join-Path $env:TEMP vs_enterprise.exe
@@ -8,6 +9,6 @@ Write-Host "Updating the installer..."
 cmd /c "`"$installer`" --update --wait --quiet"
 
 Write-Host "Installing components..."
-cmd /c "`"$installer`" modify --installPath `"$path`" --config .vsconfig --quiet --norestart --includeRecommended --wait"
+cmd /c "`"$installer`" modify --installPath `"$path`" --config $config --quiet --norestart --includeRecommended --wait"
 
 Write-Host "Installation complete."
