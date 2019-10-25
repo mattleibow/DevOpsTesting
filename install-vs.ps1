b801fc51d782f928a335d3af8c0207385a710a8f
Matthew Leibowitz <mattleibow@live.com>
2019-10-25 00:11:42 -0400

    Update install-vs.ps1

--- install-vs.ps1
+++ install-vs.ps1
@@ -1,13 +1,13 @@
-$path = & 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -property installationPath
+$path = & "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath
 Write-Host "Found Visual Studio at: $path"
 
 $installer = Join-Path $env:TEMP vs_enterprise.exe
 Invoke-WebRequest -Uri 'https://aka.ms/vs/16/release/vs_enterprise.exe' -OutFile $installer
 
 Write-Host "Updating the installer..."
-Start-Process -FilePath $installer -ArgumentList "--update --wait --quiet" -Wait -PassThru
+Start-Process -FilePath $installer -ArgumentList "--update", "--wait", "--quiet" -Wait -PassThru
 
 Write-Host "Installing components..."
-Start-Process -FilePath $installer -ArgumentList "modify --installPath $path --add Microsoft.VisualStudio.Component.VC.v141.x86.x64 --quiet --norestart --includeRecommended --wait" -Wait -PassThru
+Start-Process -FilePath $installer -ArgumentList "modify", "--installPath", $path, "--add", "Microsoft.VisualStudio.Component.VC.v141.x86.x64", "--quiet", "--norestart", "--includeRecommended", "--wait" -Wait -PassThru
 
 Write-Host "Installation complete."
