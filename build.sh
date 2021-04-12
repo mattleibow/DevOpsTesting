504621cc29b76d175f7ecb36c4a05d41e3c559ba
Matthew Leibowitz <mattleibow@live.com>
2021-04-12 03:23:24 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 xcodebuild \
     -project MyTestFramework/MyTestFramework.xcodeproj \
     -scheme MyTestFramework \
-    -configuration Release \
+    -configuration Debug \
     -arch arm64 \
     -sdk macosx \
-    -derivedDataPath $1/obj/Release/macosx/arm64 \
-    -archivePath $1/bin/Release/macosx/arm64 \
+    -derivedDataPath $1/obj \
+    -archivePath $1/bin \
     archive SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
