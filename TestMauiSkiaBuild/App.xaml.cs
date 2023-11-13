9423783dc5e5b8be10e2bc13769462d644e309c2
Matthew Leibowitz <mattleibow@live.com>
2023-11-13 19:32:41 +0200

    build

---
+++ TestMauiSkiaBuild/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TestMauiSkiaBuild;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ TestMauiSkiaBuild/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TestMauiSkiaBuild;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
