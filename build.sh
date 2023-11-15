dbd31eeae19066c3a33fbb3193212f602a7b11b6
Matthew Leibowitz <mattleibow@live.com>
2023-11-15 12:22:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-dotnet publish TestMauiSkiaBuild -f net7.0-ios -r ios-arm64 -c Release -bl:output/msbuild.binlog
+dotnet build TestMauiSkiaBuild -f net7.0-ios -r ios-arm64 -c Release -bl:output/msbuild.binlog
